8784ab43c8f9e10303396e7ea138f9f937cac67a
IVAN DUBAS <dubasyk@gmail.com>
2023-03-13 10:18:52 +0200

    Done my 2 homework

--- 2/script.js
+++ 2/script.js
@@ -1,1 +1,16 @@
-GIT 
+var myName = 'Ivan';
+var mySurname = 'Dubas';
+var myCity = 'Halych';
+var SA = (3+4+5+6)/4;
+let R = 5;
+var S,L;
+const PI = 3.14;
+
+S = PI*R*R;
+L = 2*PI*R;// довжина кола
+
+console.log ('Моє імя - ' , myName ) ;
+console.log (mySurname, '- То моє прізвище' );
+console.log ("Площа кола",S," , а його довжина", L );/* тут ми зображуємо довжину і площу
+кола*/
+
